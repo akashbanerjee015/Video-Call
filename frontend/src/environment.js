3954a44a16134eb96b6a5b1e75fa624acb7f5ae6
akashbanerjee015 <akashbanerjee7478@gmail.com>
2025-04-09 22:55:14 +0530

    refactor: simplify production mode check in environment configuration

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,16 +1,7 @@
-let IS_PROD = false;
-if (import.meta.env.MODE === "production") {
-    IS_PROD = true;
-}
-else {
-    IS_PROD = false;
-}
-;
+let IS_PROD = process.env.MODE === "production" ? true : false;
+
 const server = IS_PROD ?
     "http://localhost:8000" :
     "https://video-callbackend-hfoy.onrender.com"
-
-
-
 
 export default server;
