0805dcde526ac9726125af9b42946cb1f4c723b8
akashbanerjee015 <akashbanerjee7478@gmail.com>
2025-04-09 22:07:23 +0530

    update

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,4 @@
 let IS_PROD = false;
-if (import.meta.env.MODE === "production") {
-    IS_PROD = true
-}
 const server = IS_PROD ?
     "http://localhost:8000" :
     "https://video-callbackend-hfoy.onrender.com"
