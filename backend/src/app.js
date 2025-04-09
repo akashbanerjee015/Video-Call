a450f19ee88223f13723992efffa48f5453c5e61
akashbanerjee015 <akashbanerjee7478@gmail.com>
2025-04-09 23:23:09 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,11 @@ const io = connectToSocket(server);
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
-app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use(express.urlencoded({ limit: "40kb", extended: true })); 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
 
 app.use("/api/v1/users", userRoutes);
 
